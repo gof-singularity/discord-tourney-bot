939d0c18daac6cba46587253e7df821042ec5b89
makhambet.torezhan <makhambet.torezhan@nu.edu.kz>
2022-06-22 21:37:51 +0600

    add adding players to last tourney

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
+from secrets import token_urlsafe
 import discord
 from discord.ext import commands
 import requests
+import json
 
 client = commands.Bot(command_prefix='!')
-
 @client.event
 async def on_ready():
   print('Bot is ready')
@@ -23,13 +24,22 @@ async def createtourney(ctx, tourney):
     if ctx.author.guild_permissions.manage_channels:
         await guild.create_text_channel(name=f'{tourney}')
         response = requests.post('http://localhost:8080/tournament/create', json={ "name": f'{tourney}', "type": "round robin", "rankedBy": "points scored", "startAt": "2022-06-22T03:00:00"})
+        obj = json.loads(response.text)
+        tourney_id = obj["id"]
+
+        f=open('id.txt', 'w')
+        f.write(str(tourney_id))
+        f.close()
         await ctx.send(response.text)
 
 
 @client.command()
 async def addme(ctx):
   username = str(ctx.author).split('#')[0]
-  response = requests.post('http://localhost:8080/tournament/add-players', json = {"id": "11325133", "playerList": [f'{username}']})
+  f= open("id.txt","r")
+  tourney_id = f.read()
+  f.close()
+  response = requests.post('http://localhost:8080/tournament/add-players', json = {"id": f'{tourney_id}', "playerList": [f'{username}']})
   await ctx.send(response.text)
 
 
