0f37fb3ce08a3b6f93915f679179e56becc6b811
yernazarov <zhandos.yernazarov@nu.edu.kz>
2022-06-23 14:32:31 +0600

    Add 'set_winner', 'get_matches', 'start_tournament' features

---
+++ server.py
@@ -0,0 +1,33 @@
+import requests, json
+def get_matches(tournament_id):
+  matches = []
+  response = requests.get(f'https://api.challonge.com/v1/tournaments/{tournament_id}/matches.json', params={"api_key": "q1zaMKGU0PGgNoL2DzZJLXGHXiaQLMFMAM4Huxap"},
+                          headers={"User-Agent": "PostmanRuntime/7.29.0"})
+  data_json = json.loads(response.text)
+  for match in data_json:
+      matches.append({"id": match['match']['id'], 'player1_id': match['match']['player1_id'],
+                "player2_id": match['match']["player2_id"], "round": match['match']['round'],
+                "winner_id": match['match']["winner_id"]})
+  return matches
+
+def set_winner(tournament_id, match_id, winner_id: str):
+    match = json.loads(requests.get(f'https://api.challonge.com/v1/tournaments/{tournament_id}/matches/{match_id}.json',
+                               params={"api_key": "q1zaMKGU0PGgNoL2DzZJLXGHXiaQLMFMAM4Huxap"},
+                               headers={"User-Agent": "PostmanRuntime/7.29.0"}).text)
+    if match['match']['player1_id'] == winner_id:
+        score = "1-0"
+    else: score = "0-1"
+    response = requests.put(f'https://api.challonge.com/v1/tournaments/{tournament_id}/matches/{match_id}.json',
+                          params={"api_key": "q1zaMKGU0PGgNoL2DzZJLXGHXiaQLMFMAM4Huxap",
+                                  "match[winner_id]": winner_id, "match[scores_csv]": score},
+                          headers={"User-Agent": "PostmanRuntime/7.29.0"})
+    data_json = json.loads(response.text)
+    return data_json
+
+def start_tournament(tournament_id):
+    response = requests.post(f'https://api.challonge.com/v1/tournaments/{tournament_id}/start.json',
+                             params={"api_key": "q1zaMKGU0PGgNoL2DzZJLXGHXiaQLMFMAM4Huxap"},
+                             headers={"User-Agent": "PostmanRuntime/7.29.0"})
+    return json.loads(response.text)
+
+print(set_winner(11329318, 280540812, 174301808))
